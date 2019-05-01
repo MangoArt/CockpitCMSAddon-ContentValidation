4d534e328ee60f64f1283e3ddf0b44ee50fed9c9
Markus Oehler <m.oehler@digital-city.solutions>
2019-05-01 12:12:05 +0200

    Code cleanup Add support for CollectionLink fields with multiple entries

--- bootstrap.php
+++ bootstrap.php
@@ -5,92 +5,6 @@
  * Implements bootstrap functions.
  */
 
-function validateFields($app, $name, $fields, $data) {
-    $errors = [];
-    $errorData = [
-        'module' => 'Collection',
-        'entity' => $name,
-        '_id' => $data['_id'],
-        'name' => ($data['name'] ?? $data['title'] ?? $data[$fields[0]['name']]),
-    ];
-
-    foreach($fields as $field) {
-        $errors = array_merge($errors, validateField($app, $field, $data[$field['name']], $errorData));
-    }
-
-    return $errors;
-}
-
-function validateField($app, $field, $fieldData, $errorData) {
-    $errors = [];
-    if (array_key_exists('required', $field) && $field['required'] && !$fieldData) {
-        $errors []= array_merge([
-            'violationtype' => 'Error',
-            'violation' => 'Required field ' . $field['label'] . ' [' . $field['name'] . '] does not have a value'
-        ], $errorData);
-    }
-
-    if ($field['type'] === 'image' && $fieldData) {
-        $path = $fieldData['path'];
-        if (empty($path) && $field['required']) {
-            $errors []= array_merge([
-                'violationtype' => 'Error',
-                'violation' => 'Required field ' . $field['label'] . ' [' . $field['name'] . '] does not have a image path specified'
-            ], $errorData);
-        } else if (stripos($path, 'http') === 0) {
-            if (!is_url_exist($path)) {
-                $errors []= array_merge([
-                    'violationtype' => 'Error',
-                    'violation' => 'Field ' . $field['label'] . ' [' . $field['name'] . '] contains invalid image url "' . $path . '" that cannot be resolved!'
-                ], $errorData);
-            }
-        } else if (!file_exists($path)) {
-            $errors []= array_merge([
-                'violationtype' => 'Error',
-                'violation' => 'Field ' . $field['label'] . ' [' . $field['name'] . '] contains invalid image url "' . $path . '" that cannot be resolved!'
-            ], $errorData);
-        } else if (stripos($path, '.gif')) {
-            $errors []= array_merge([
-                'violationtype' => 'Warning',
-                'violation' => 'Field ' . $field['label'] . ' [' . $field['name'] . '] contains a GIF image which cannot be processed by the Sharp Image processing library used in Gatsby'
-            ], $errorData);
-        }
-    }
-
-
-    if ($field['type'] === 'collectionlink' && $fieldData) {
-        $collections = $app->module('collections')->collections();
-        $collectionFound = false;
-        $itemFound = false;
-        foreach ($collections as $name => $collection) {
-            if ($name === $fieldData['link']) {
-                $collectionFound = true;
-                $items = $app->storage->find("collections/" . $collection['_id'])->toArray();
-                foreach ($items as $item) {
-                    if ($item["_id"] === $fieldData["_id"]) {
-                        $itemFound = true;
-                    }
-                }
-            }
-        }
-
-        if (!$collectionFound) {
-            $errors []= array_merge([
-                'violationtype' => 'Error',
-                'violation' => 'Field ' . $field['label'] . ' [' . $field['name'] . '] contains a link to a Collection that doesn\'t exist any more!'
-            ], $errorData);
-        } else if (!$itemFound) {
-            $errors []= array_merge([
-                'violationtype' => 'Error',
-                'violation' => 'Field ' . $field['label'] . ' [' . $field['name'] . '] contains a link to a collection item that doesn\'t exist any more!'
-            ], $errorData);
-        }
-    }
-
-    return $errors;
-}
-
-
 function is_url_exist($url){
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_NOBODY, true);
@@ -99,7 +13,7 @@ function is_url_exist($url){
 
     if($code == 200){
         $status = true;
-    }else{
+    } else {
         $status = false;
     }
     curl_close($ch);
@@ -108,33 +22,141 @@ function is_url_exist($url){
 
 // Include addon functions only if its an admin request.
 if (COCKPIT_ADMIN && !COCKPIT_API_REQUEST) {
-  // Extend addon functions.
-  $this->module('errorcheck')->extend([
-    'validate' => function () {
-        // Save collections.
+    // Extend addon functions.
+    $this->module('errorcheck')->extend([
+        'validate' => function () {
+            $errors = $this->validateCollections();
+            return $errors;
+        },
 
-      $result = [];
-      $collections = $this->app->module('collections')->collections();
-      foreach ($collections as $name => $collection) {
-        $cid = $collection['_id'];
-        $fields = $collection['fields'];
-        $items = $this->app->storage->find("collections/{$cid}")->toArray();
+        'validateCollections' => function() {
+            $errors = [];
+            $collections = $this->app->module('collections')->collections();
+            foreach ($collections as $name => $collection) {
+                $errors = $this->validateCollection($name, $collection);
+            }
+            return $errors;
+        },
 
-        if (count($items)) {
-          foreach($items as $item) {
-            $result = array_merge($result, validateFields($this->app, $name, $fields, $item));
-          }
-        }
-      }
+        'validateCollection' => function($name, $collection) {
+            $cid = $collection['_id'];
+            $fields = $collection['fields'];
+            $items = $this->app->storage->find("collections/{$cid}")->toArray();
+            $errors = [];
 
-        return $result;
-    },
+            foreach($items as $item) {
+                $errors = array_merge($errors, $this->validateFields($this->app, $name, $fields, $item));
+            }
+            return $errors;
+        },
 
-    'createDeploy' => function () {
-      return null;
-    },
-  ]);
+        'validateFields' => function($name, $fields, $data) {
+            $errors = [];
+            $errorData = [
+                'module' => 'Collection',
+                'entity' => $name,
+                '_id' => $data['_id'],
+                'name' => ($data['name'] ?? $data['title'] ?? $data[$fields[0]['name']]),
+            ];
 
-  // Include admin.
-  include_once __DIR__ . '/admin.php';
+            foreach($fields as $field) {
+                $errors = array_merge($errors, $this->validateField($field, $data[$field['name']], $errorData));
+            }
+
+            return $errors;
+        },
+
+        'validateField' => function($field, $fieldData, $errorData) {
+            $errors = [];
+            if (array_key_exists('required', $field) && $field['required'] && !$fieldData) {
+                $errors []= array_merge([
+                    'violationtype' => 'Error',
+                    'violation' => 'Required field ' . $field['label'] . ' [' . $field['name'] . '] does not have a value'
+                ], $errorData);
+            }
+
+            if ($field['type'] === 'image' && $fieldData) {
+                array_merge($errors, $this->validateImageField($field, $fieldData, $errorData));
+            } else if ($field['type'] === 'collectionlink' && $fieldData) {
+                array_merge($errors, $this->validateCollectionLinkField($field, $fieldData, $errorData));
+            }
+
+            return $errors;
+        },
+
+        'validateImageField' => function($field, $fieldData, $errorData) {
+            $errors = [];
+            $path = $fieldData['path'];
+            if (empty($path) && $field['required']) {
+                $errors []= array_merge([
+                    'violationtype' => 'Error',
+                    'violation' => 'Required field ' . $field['label'] . ' [' . $field['name'] . '] does not have a image path specified'
+                ], $errorData);
+            } else if (stripos($path, 'http') === 0) {
+                if (!is_url_exist($path)) {
+                    $errors []= array_merge([
+                        'violationtype' => 'Error',
+                        'violation' => 'Field ' . $field['label'] . ' [' . $field['name'] . '] contains invalid image url "' . $path . '" that cannot be resolved!'
+                    ], $errorData);
+                }
+            } else if (!file_exists(COCKPIT_DIR . '/' .$path)) {
+                $errors []= array_merge([
+                    'violationtype' => 'Error',
+                    'violation' => 'Field ' . $field['label'] . ' [' . $field['name'] . '] contains invalid image url "' . $path . '" that cannot be resolved!'
+                ], $errorData);
+            } else if (stripos($path, '.gif')) {
+                $errors []= array_merge([
+                    'violationtype' => 'Warning',
+                    'violation' => 'Field ' . $field['label'] . ' [' . $field['name'] . '] contains a GIF image which cannot be processed by the Sharp Image processing library used in Gatsby'
+                ], $errorData);
+            }
+            return $errors;
+        },
+
+        'validateCollectionLinkField' => function($field, $fieldData, $errorData) {
+            $collections = $this->app->module('collections')->collections();
+            $collectionFound = false;
+            $itemFound = false;
+            $errors = [];
+
+            if (array_key_exists('_id', $fieldData)) {
+                $entries = [$fieldData];
+            } else {
+                $entries = $fieldData;
+            }
+            foreach($entries as $entry) {
+                foreach ($collections as $name => $collection) {
+                    if ($name === $entry['link']) {
+                        $collectionFound = true;
+                        $items = $this->app->storage->find("collections/" . $collection['_id'])->toArray();
+                        foreach ($items as $item) {
+                            if ($item["_id"] === $entry["_id"]) {
+                                $itemFound = true;
+                            }
+                        }
+                    }
+                }
+
+                if (!$collectionFound) {
+                    $errors [] = array_merge([
+                        'violationtype' => 'Error',
+                        'violation' => 'Field ' . $field['label'] . ' [' . $field['name'] . '] contains a link to a Collection that doesn\'t exist any more!'
+                    ], $errorData);
+                } else if (!$itemFound) {
+                    $errors [] = array_merge([
+                        'violationtype' => 'Error',
+                        'violation' => 'Field ' . $field['label'] . ' [' . $field['name'] . '] contains a link to a collection item that doesn\'t exist any more!'
+                    ], $errorData);
+                }
+            }
+            return $errors;
+        },
+
+        'createDeploy' => function () {
+            return null;
+        },
+    ]);
+
+    // Include admin.
+    include_once __DIR__ . '/admin.php';
 }
